29ebbd24969c6f36d4780cebab3f1cbf1b60dd4d
MAGESH H <70838644+mageshyt@users.noreply.github.com>
2022-08-07 19:31:51 +0530

    function and function overloading added 🙂

--- Functions/Function.java
+++ Functions/Function.java
@@ -3,9 +3,9 @@ public class Function {
         // function is a block of code that performs a specific task
         sayHello("Magesh & Lara");
         sayHello("Hemanth & Ranjith");
-        sayHello("Sathivika");
-        sayHello("Nabe &devi");
         sayHello("Amen & sindhu");
+        sayHello("Nabe &devi");
+        sayHello("Sathivika");
 
         int a = 10;
         int b = 20;
